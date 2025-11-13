f956a665bf027f8c437679063f3d0fc27f88ad77
prolinked24-source <prolinked24@gmail.com>
2025-11-13 09:17:44 +0100

    Update 2025_11_13_075822_create_employers_table.php

--- backend/database/migrations/2025_11_13_075822_create_employers_table.php
+++ backend/database/migrations/2025_11_13_075822_create_employers_table.php
@@ -10,18 +10,24 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('employers', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('employers', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->string('company_name');
+        $table->string('contact_person')->nullable();
+        $table->string('country')->nullable();
+        $table->string('city')->nullable();
+        $table->string('industry')->nullable();
+        $table->string('website')->nullable();
+        $table->text('company_description')->nullable();
+        $table->timestamps();
+    });
+}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('employers');
-    }
+public function down(): void
+{
+    Schema::dropIfExists('employers');
+}
+
 };
