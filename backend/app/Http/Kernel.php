69c4063afa979d337a18da7cd33ccfea84b6e2f7
Abdel El-Boujattoui <prolinked24@gmail.com>
2025-11-13 08:04:08 +0000

    Initial public setup

---
+++ backend/app/Http/Kernel.php
@@ -0,0 +1,6 @@
+protected $middlewareGroups = [
+    'api' => [
+        \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        \Illuminate\Routing\Middleware\SubstituteBindings::class,
+    ],
+];
