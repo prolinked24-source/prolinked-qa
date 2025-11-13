a9d34a9718b79ef1033c1307cbaf35151cec0143
prolinked24-source <prolinked24@gmail.com>
2025-11-13 09:36:08 +0100

    Update Job.php

--- backend/app/Models/Job.php
+++ backend/app/Models/Job.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model
 {
-    //
+    protected $fillable = [
+        'employer_id',
+        'title',
+        'location',
+        'employment_type',
+        'description',
+        'requirements',
+        'language_requirement',
+        'is_active',
+    ];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function applications()
+    {
+        return $this->hasMany(Application::class);
+    }
 }
