4250b45456f10d3a2b717529d1ec842389a7c5ab
prolinked24-source <prolinked24@gmail.com>
2025-11-13 09:36:51 +0100

    Update Application.php

--- backend/app/Models/Application.php
+++ backend/app/Models/Application.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Application extends Model
 {
-    //
+    protected $fillable = [
+        'candidate_profile_id',
+        'job_id',
+        'status',
+        'notes',
+    ];
+
+    public function candidateProfile()
+    {
+        return $this->belongsTo(CandidateProfile::class);
+    }
+
+    public function job()
+    {
+        return $this->belongsTo(Job::class);
+    }
 }
